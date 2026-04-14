6a64f3f609bbe463d245083367f9ec8682b52d32
HariWorkSpace <135123281+harinaidu89@users.noreply.github.com>
2026-04-14 09:18:41 +0530

    Datastructures
    Datastructures

---
+++ data-structures-algorithms/src/main/java/com/learning/datastructuresalgorithms/datastructures/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,14 @@
+package com.learning.datastructuresalgorithms.datastructures.linkedlist;
+
+import java.util.LinkedList;
+public class SinglyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("Singly Linked List");
+
+    }
+}
+class Node{
+    Integer data;
+    Node next;
+
+}
---
+++ data-structures-algorithms/src/test/java/com/learning/datastructuresalgorithms/DataStructuresAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.datastructuresalgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresAlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
